7e376283594e09e8ddfcc7a3fceef0d67037eab3
Fernando G <fgomez@masterdevel.com>
2019-05-02 15:17:30 -0600

    Updates

---
+++ precache-manifest.cebbc1d3e8e56ae6d2460fe38a7903f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "58204facf0f9a289c127",
+    "url": "/robo-friends/static/js/main.8f165d57.chunk.js"
+  },
+  {
+    "revision": "2809f1bd6829e036a911",
+    "url": "/robo-friends/static/js/2.cc368224.chunk.js"
+  },
+  {
+    "revision": "58204facf0f9a289c127",
+    "url": "/robo-friends/static/css/main.d88351b2.chunk.css"
+  },
+  {
+    "revision": "2809f1bd6829e036a911",
+    "url": "/robo-friends/static/css/2.e1cd78c2.chunk.css"
+  },
+  {
+    "revision": "6ff5ea40ad5502f55d43b4413143804a",
+    "url": "/robo-friends/index.html"
+  }
+];
